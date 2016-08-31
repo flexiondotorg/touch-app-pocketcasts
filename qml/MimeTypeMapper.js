2f3b6051d319b61dd86de454c94b97b4c73dd5c0
Martin Wimpress <code@flexion.org>
2016-08-31 10:48:27 +0100

    Update license header.

--- qml/MimeTypeMapper.js
+++ qml/MimeTypeMapper.js
@@ -1,13 +1,13 @@
 /*
  * Copyright 2014 Canonical Ltd.
  *
- * This file is part of webbrowser-app.
+ * This file is part of touch-app-pocketcasts.
  *
- * webbrowser-app is free software; you can redistribute it and/or modify
+ * touch-app-pocketcasts is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; version 3.
  *
- * webbrowser-app is distributed in the hope that it will be useful,
+ * touch-app-pocketcasts is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
